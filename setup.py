ab29518ff7215b013547dd0e2de5473309f5ec1d
Jim Fleming <jimmy.fleming@gmail.com>
2017-04-28 14:51:31 -0700

    Update setup module to support pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
+"""
+Setup module.
+"""
+
+from os import path
 from setuptools import setup
+
+REQUIRED_PACKAGES = []
+HERE = path.abspath(path.dirname(__file__))
+
+with open(path.join(HERE, 'README.rst'), encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
 
 setup(name='tfstage',
       version='0.1.0',
       description='TensorFlow project scaffolding',
+      long_description=LONG_DESCRIPTION,
       url='http://github.com/fomorians/tfstage',
       author='Jim Fleming',
       author_email='jim@fomoro.com',
