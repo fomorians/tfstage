3286a4645cee987c42dbeeb644b4eb1127c9f0b2
Jim Fleming <jimmy.fleming@gmail.com>
2017-07-05 11:12:50 -0700

    Bump package version

--- tfstage/__init__.py
+++ tfstage/__init__.py
@@ -2,6 +2,6 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
 
-__version__ = '0.1.0'
+__version__ = '0.1.5'
 
 from tfstage.tfstage import generate_project
